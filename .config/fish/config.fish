f05204e3d1d3ceae30840fbf96b283f7112b04c2
thomas <t108.servant@gmail.com>
2026-03-14 16:48:02 +0100

    enable vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path ~/.local/bin/
     alias rmi "rm -i"
+    set -g fish_key_bindings fish_vi_key_bindings
 end
